d5c1610a599a23b4110918f3562122293314d825
GreatestTrain <96893357+GreatestTrain@users.noreply.github.com>
2022-09-27 13:31:07 -0500

    Update install.sh
    aaaa

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 BLUE="\033[0;34m"
 CYAN="\033[0;36m"
-NC='\033[0m' # No Color
+NC="\033[0m" # No Color
 
 function error () { printf "${RED}$1${NC}\n"; return 1; }
 function success () { printf "${GREEN}$1${NC}\n"; return 0; }
